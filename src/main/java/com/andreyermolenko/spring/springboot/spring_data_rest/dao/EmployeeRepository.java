98395d2c8adab4a2fdb325dd763f03d12dbe89a8
Andrey <xramovic@yandex.ru>
2022-02-26 11:35:55 +0600

    init

---
+++ src/main/java/com/andreyermolenko/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.andreyermolenko.spring.springboot.spring_data_rest.dao;
+
+
+
+import com.andreyermolenko.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer > {
+
+}
